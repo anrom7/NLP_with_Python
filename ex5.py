33abd9d824086ae2e87f0d20738fc98b0badd516
Andriy Romanyuk <anrom7@gmail.com>
2013-03-16 14:06:57 +0200

    Del files

--- ex5.py
+++
@@ -1,4 +0,0 @@
-import nltk
-d = {'chair' : 'table', 'window' : 'door'} #dictionary d, and add some entries
-d['flower'] = 'rose' #add new value
-print d['xyz']  #we get an error here "KeyError: 'xyz'"
