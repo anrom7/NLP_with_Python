97e45654c1dbf8b2231a0c9fdff6e8396610aba9
Andriy Romanyuk <anrom7@gmail.com>
2013-03-26 14:23:49 +0200

    Del files from master branch
    Please add files to your branch

--- Komar/5.10_5_dic.py
+++
@@ -1,7 +0,0 @@
-#Komar Mariia ALs-11 chapter_5 Ex_5
-#Using the Python interpreter in interactive mode, experiment with the dictionary. Create a dictionary d, and add some entries. What happens whether you try access a non-existent entry, e.g., d['xyz']?
-
-import nltk
-from nltk.corpus import brown 
-d = {'charming': 'ADJ', 'home': 'N', 'go': 'V', 'slowly': 'ADV'} # add new words to the dictionary 
-print d ['xvz'] #access a non-existent entry d['xyz']
