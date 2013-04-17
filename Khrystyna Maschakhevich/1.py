fd039ace1d388d9804701a278dab7133a6adccf5
Andriy Romanyuk <anrom7@gmail.com>
2013-04-17 13:51:57 +0300

    del files

--- Khrystyna Maschakhevich/1.py
+++
@@ -1,5 +0,0 @@
-## Khrystyna Maschakhevich, AL-13, Chapter 6, Tast 1: the questin was: Find out what type and quantity of annotated data is required for developing such systems. Why do you think a large amount of data is required?
-## Modeling of corpus linguistic data can help us to understtand the language models and is used to predict new language data.
-## Classifiers use trained corpuses for building models, taking to consideration the specific input, they can perform a wide range of tasks, including
-##classification of documents, definition of language and segmenting of sentences.
-##It is important to use a large amount of new data which was not trained previously. Otherwise the result can be not justified
