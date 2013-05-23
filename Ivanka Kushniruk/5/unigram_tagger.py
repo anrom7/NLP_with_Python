cfc8c4b04f55f3f6a03ae803dfcdec69dfe7dab4
Andriy Romanyuk <anrom7@gmail.com>
2013-05-23 17:50:52 +0300

    Del files

--- Ivanka Kushniruk/5/unigram_tagger.py
+++
@@ -1,9 +0,0 @@
-#Ivanna Kushniruk
-import nltk
-from nltk.corpus import brown
-brown_reviews=brown.tagged_sents(categories='reviews')#returns a list of tagged sentences
-brown_sents=brown.sents(categories='reviews') # and have pure sents without tags
-unigram_tagger=nltk.UnigramTagger(brown_reviews) # build unigram tagger
-print unigram_tagger.tag(brown_sents[3]) # check 3rd sentence
-print unigram_tagger.evaluate(brown_reviews) # evaluation
-print 'there is no untagged word in this sentence, but this might happen when the tagger has not seen the word before so this word will receive tag of None'
--- Ivanka Kushniruk/5/working with dictionaries.py
+++
@@ -1,8 +0,0 @@
-# by Ivanna Kushniruk
-pos = {'colorless': 'ADJ', 'ideas': 'N', 'sleep': 'V', 'furiously': 'ADV'} # copied dictionary from the book
-print pos.keys() #checking list of keys
-d = {'cat':'N', 'black':'ADJ','dog':'N','go':'V'} # created my dictionary
-print d ['go'] # checking value
-d.update(pos) # united two dictionaries
-print d ['colorless'] # checked value from dict 'pos' if it is in dict 'd'
-print d ['xyz'] # error will apear as 'xyz' is not in any dictionary
