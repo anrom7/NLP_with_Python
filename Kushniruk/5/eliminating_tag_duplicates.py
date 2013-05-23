32d010133ef0f5dab9b7fd2bd8f194e3611f3079
Ivanka Kushniruk <ivanna.kushniruk@gmail.com>
2013-05-23 10:34:56 +0300

    -

--- Kushniruk/5/eliminating_tag_duplicates.py
+++
@@ -1,7 +0,0 @@
-#by Ivanna Kushniruk
-import nltk
-paru=nltk.corpus.brown.tagged_words()
-tags=[tag for (word, tag) in paru] # extract all tags from pairs 'word,tag'
-w=set(tags) # sorting with repetition of tags
-print sorted(w) [:20] # sorting sorted w and eliminating duplicates
-
