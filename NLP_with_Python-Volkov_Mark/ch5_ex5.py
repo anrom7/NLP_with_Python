0254c5ba96c0ace385c30630f1654c30b79396cb
Svyatoslav <svyatoslav.sydorenko@mail.ru>
2013-06-25 23:01:12 +0300

    Revert "EXERCISES"
    This reverts commit f6483195d46ea9bdf2a23beaabcad38aa25f7849.

--- NLP_with_Python-Volkov_Mark/ch5_ex5.py
+++
@@ -1,7 +0,0 @@
-#presented by Volkov Mark PRLc 11
-# Chapter 9, Ex.2
-
-import nltk
-d = {'chair' : 'table', 'window' : 'door'} #dictionary d, and add some entries
-d['flower'] = 'rose' #add new value
-print d['xyz']  #we get an error here "KeyError: 'xyz'"
