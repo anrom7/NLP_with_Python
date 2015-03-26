dd87b00e42c5d918e472b0d0ec7d0a1ca2cd8d99
Andriy Romanyuk <anrom7@gmail.com>
2015-03-26 10:02:03 +0200

    Revert "Revert "Add task 5.11 from""
    This reverts commit bae1118556ffa3e9830d92320d88567dccdc2cc8.

---
+++ Romanyuk/find_word_brown.py
@@ -0,0 +1,15 @@
+﻿#
+#
+#
+#
+#
+import nltk
+from nltk.corpus import brown
+def find_word(word):
+    """ Функція для пошуку слова в жанрі корпуса Brown
+
+    """
+    for ganre in brown.categories():
+        if word in brown.words(categories=ganre):
+            print word
+
