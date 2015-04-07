1dd1183a0b66500a73d02bbbc39ad4144ad7eb2d
svitlana synytsya <svitlana.synytsta@gmail.com>
2015-04-07 15:41:02 +0300

    6_1

---
+++ 6_1.py
@@ -0,0 +1,24 @@
+# Chapter 6
+# Exercise 1
+# Author Svitlana Synytsia
+'''
+Read up on one of the language technologies mentioned in this section, such as
+word sense disambiguation, semantic role labeling, question answering, machine
+translation, or named entity recognition. Find out what type and quantity of annotated
+data is required for developing such systems. Why do you think a large
+amount of data is required?
+'''
+#Word sense disambiguation is a challenging field in natural language processing. 
+#There are a number of different approaches to the problem, but currently the most 
+#accurate involve supervised methods to train taggers using annotated corpora. 
+#A large quantity of annotated data is needed to develop such software due to the 
+#complexity of the problem and various contexts in which a given word can occur. 
+#As with part of speach tagging, the more data a tagger sees in training, 
+#the more accurate it will be when asked to process new data.
+# Word sense disambiguation (WSD, or Word sense discrimination) needs such type of data as
+# - annotated corpora
+# - labeled training data sets
+# - desicion rules
+# - set of high-level classifiers with smart boundaries
+# Most of WSD methods require large quantities of manually annotated training data in order to
+#reach high accuracy of WSD.
