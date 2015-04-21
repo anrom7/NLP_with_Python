abae02bd61bd0b24f8868ba189c337ad739a3f0b
hentoshka <uliana.hentosh@gmail.com>
2015-04-21 16:41:51 +0300

    Revert "Chapter 5"
    This reverts commit a4f933272c86130df02dafe02754c6df4930c5c9.

--- 5_24.py
+++
@@ -1,7 +0,0 @@
-import nltk
-from nltk.corpus import brown
-tagged=brown.tagged_sents(categories='learned', simplify_tags=True)
-train_data=tagged
-for w in range (1,6):
-	ngram=nltk.NgramTagger (w, tagged)
-	print ('ngram, n=', w, ngram.evaluate(nltk.corpus.brown.tagged ()))
