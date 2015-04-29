83fdeb5c57d877571fd4b8c0aedbae4dac3c253e
NataliaVoitsiv Prls-12 <netali1303@gmail.com>
2015-04-29 23:25:19 +0300

    Chapter 5

---
+++ Chapter 5/ch5_ex5.py
@@ -0,0 +1,7 @@
+#Voitsiv Natalia PRLS-12
+# ex.5 
+
+import nltk
+d = {'chair' : 'table', 'window' : 'door'} #dictionary d, and add some entries
+d['flower'] = 'rose' #add new value
+print d['xyz']  #we get an error here "KeyError: 'xyz'"
