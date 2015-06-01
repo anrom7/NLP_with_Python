96d474cad494e857b35c35a09df32207f67b604c
politehGitHub <ms.masiuk@mail.ru>
2015-06-01 13:22:07 +0300

    chapter6__1

---
+++ chapter6_1.py
@@ -0,0 +1,6 @@
+# Prizvuszcze / Grypa
+
+# Modeling of corpus linguistic data can help us to understtand the language models and is used to predict new language data.
+# Classifiers use trained corpuses for building models, taking to consideration the specific input, they can perform a wide range of tasks, including
+# classification of documents, definition of language and segmenting of sentences.
+# It is important to use a large amount of new data which was not trained previously. Otherwise the result can be not justified
