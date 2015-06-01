410bd79953694dc7dc99d867ba351c30fc04e06e
hentoshka <uliana.hentosh@gmail.com>
2015-06-01 23:00:09 +0300

    Розділ 7

---
+++ Розділ 7/7_4.py
@@ -0,0 +1,12 @@
+#Uliana Hentosh
+# Ddeveloping the chunker that starts by putting the whole sentence in a single chunk
+
+import nltk
+grammar = r"""
+  NP:
+  {<.*>+}
+  }<VBD|IN>+{
+  """
+whole_sent = [("the", "DT"), ("shiny", "JJ"), ("hot", "JJ"), ("weather", "NN"), ("changes", "VBD"), ("into", "IN"), ("suffering", "NN"), ("for", "IN"), ("us", "PRP")]
+single = nltk.RegexpParser(grammar)
+print 'A single chunk:  ', single.parse(whole_sent)
