0b44694970227a4d935bfdf8d3717f8baac782be
politehGitHub <ms.masiuk@mail.ru>
2015-06-01 09:31:15 +0300

    chapter_5_23

---
+++ 5_23.py
@@ -0,0 +1,21 @@
+import nltk
+from nltk.corpus import brown
+patterns = [
+    (r'.*ing$', 'VBG'), # gerunds
+    (r'.*ed$', 'VBD'), # simple past
+    (r'.*es$', 'VBZ'), # 3rd singular present
+    (r'.*ould$', 'MD'), # modals
+    (r'.*\'s$', 'NN$'), # possessive nouns
+    (r'.*s$', 'NNS'), # plural nouns
+    (r'^-?[0-9]+(.[0-9]+)?$', 'CD'), # cardinal numbers
+    (r'.*', 'NN') # nouns (default)
+    ]
+regexp_tagger = nltk.RegexpTagger(patterns)
+regexp_tagger.tag(brown.sents()[10])
+print regexp_tagger.tag(brown.sents()[10]) #sentence
+brown_test=brown.tagged_sents()[:50]
+100.0 * regexp_tagger.evaluate(brown.tagged_sents()[50:500])
+print 100.0 * regexp_tagger.evaluate(brown.tagged_sents()[50:500]) #Accuracy of Regular expressions Tagger
+two_tagger=nltk.UnigramTagger(brown_test, backoff=regexp_tagger)
+100.0 * two_tagger.evaluate(brown.tagged_sents()[50:500])
+print 100.0 * two_tagger.evaluate(brown.tagged_sents()[50:500]) #Accuracy of Regular expressions Tagger + Unigram tagger
