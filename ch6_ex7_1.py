cd1a9a22489d6cb1cf1a69177235f3d79a3ada14
Mary Keldysh <anna.gadzalo@gmail.com>
2015-06-03 16:42:24 +0300

    chapter 6
    ex7_revised

---
+++ ch6_ex7_1.py
@@ -0,0 +1,27 @@
+# Anna Hadzalo
+#PRLS-11
+#Chapter 6
+# Task 7
+
+import nltk
+posts=nltk.corpus.nps_chat.xml_posts() # here we import posts
+history=[]
+def dialogue_act_features(post,i,history):  #creating function of obtaining features
+        features={}
+        for word in nltk.word_tokenize(post):
+                features['contains(%s)' %word.lower()]=True
+        if i==0:
+                features["prev-class"]=""
+        else:
+                features["prev-class"]=history[i-1]
+        return features
+
+
+featuresets=[]
+for i,post in enumerate(posts): 
+        featuresets.append((dialogue_act_features(post.text,i,history),post.get('class')))
+        history.append(post.get('class'))
+size=int(len(featuresets)*0.1)
+train_set, test_set=featuresets[size:],featuresets[:size]
+classifier=nltk.NaiveBayesClassifier.train(train_set) 
+print nltk.classify.accuracy(classifier, test_set)
