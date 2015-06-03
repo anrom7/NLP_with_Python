aa90959a5f9ab68ce712dced3f2c2dc61a38c9c5
Mary Keldysh <anna.gadzalo@gmail.com>
2015-06-03 16:52:05 +0300

    chapter 7

---
+++ ch7_ex3.py
@@ -0,0 +1,15 @@
+# Anna Hadzalo
+#PRLS-11
+#Chapter 7
+# Task 3
+
+import nltk
+from nltk.corpus import conll2000
+grammar="NP: {<DT|JJ>?<RB>?<VBN>?<NN.*>+}" 
+sent='Either way, I very much doubt that writing schools give any space to the idea that there are some passages in great novels that are so profoundly boring they should not detain our attention.'
+sentence=sent.split() # devided sentence into single words
+tagged=nltk.pos_tag(sentence) #tagged the words
+cp=nltk.RegexpParser(grammar) # developed a simple chunker
+result=cp.parse(tagged)
+print result
+
